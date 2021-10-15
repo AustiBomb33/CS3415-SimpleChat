dd611afd21efea15943b432db56dec7e12924360
BoolAid-Man <marcouxaustin@gmail.com>
2021-10-15 14:10:30 -0400

    Server can now send its own messages and has a UI console

--- src/simplechat1/EchoServer.java
+++ src/simplechat1/EchoServer.java
@@ -2,9 +2,11 @@ package simplechat1;// This file contains material supporting section 3.7 of the
 // "Object Oriented Software Engineering" and is issued under the open-source
 // license found at www.lloseng.com 
 
-import ocsf.server.*;
+import ocsf.server.ConnectionToClient;
+import simplechat1.common.ChatIF;
+
 /**
- * This class overrides some of the methods in the abstract 
+ * This class overrides some of the methods in the abstract
  * superclass in order to give more functionality to the server.
  *
  * @author Dr Timothy C. Lethbridge
@@ -13,106 +15,105 @@ import ocsf.server.*;
  * @author Paul Holden
  * @version July 2000
  */
-public class EchoServer extends ocsf.server.AbstractServer
-{
-  //Class variables *************************************************
-  
-  /**
-   * The default port to listen on.
-   */
-  final public static int DEFAULT_PORT = 5555;
-  
-  //Constructors ****************************************************
-  
-  /**
-   * Constructs an instance of the echo server.
-   *
-   * @param port The port number to connect on.
-   */
-  public EchoServer(int port) 
-  {
-    super(port);
-  }
+public class EchoServer extends ocsf.server.AbstractServer {
+    //Class variables *************************************************
 
-  
-  //Instance methods ************************************************
+    /**
+     * The default port to listen on.
+     */
+    final public static int DEFAULT_PORT = 5555;
 
+    public static ChatIF serverUI;
+
+    //Constructors ****************************************************
+
+    /**
+     * Constructs an instance of the echo server.
+     *
+     * @param port The port number to connect on.
+     */
+    public EchoServer(int port) {
+        super(port);
+    }
+
+    public EchoServer(int port, ChatIF ui) {
+        super(port);
+        serverUI = ui;
+    }
+
+    /**
+     * This method is responsible for the creation of
+     * the server instance (there is no UI in this phase).
+     *
+     * @param args [0] The port number to listen on.  Defaults to 5555
+     *             if no argument is entered.
+     */
+    public static void main(String[] args) {
+        int port = 0; //Port to listen on
+
+        try {
+            port = Integer.parseInt(args[0]); //Get port from command line
+        } catch (Throwable t) {
+            port = DEFAULT_PORT; //Set port to 5555
+        }
+
+        EchoServer sv = new EchoServer(port);
+
+        try {
+            sv.listen(); //Start listening for connections
+        } catch (Exception ex) {
+            System.out.println("ERROR - Could not listen for clients!");
+        }
+    }
+
+    //Instance methods ************************************************
+    public void handleMessageFromServer(String message) {
+        this.sendToAllClients("SERVER MSG> " + message);
+        serverUI.display("SERVER MSG> " + message);
+    }
 
     @Override
     public void clientConnected(ConnectionToClient client) {
+        serverUI.display("Client connected from " + client);
         this.sendToAllClients("Welcome to the server! What's your name?");
     }
 
     @Override
     protected synchronized void clientDisconnected(ConnectionToClient client) {
+        serverUI.display("Client disconnected from " + client);
         this.sendToAllClients("Somebody left. Goodbye!");
     }
 
-  /**
-   * This method handles any messages received from the ocsf.client.
-   *
-   * @param msg The message received from the ocsf.client.
-   * @param client The connection from which the message originated.
-   */
-  public void handleMessageFromClient
-    (Object msg, ConnectionToClient client)
-  {
-    System.out.println("Message received: " + msg + " from " + client);
-    this.sendToAllClients(msg);
-  }
-    
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server starts listening for connections.
-   */
-  protected void serverStarted()
-  {
-    System.out.println
-      ("Server listening for connections on port " + getPort());
-  }
-  
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server stops listening for connections.
-   */
-  protected void serverStopped()
-  {
-    System.out.println
-      ("Server has stopped listening for connections.");
-  }
-  
-  //Class methods ***************************************************
-  
-  /**
-   * This method is responsible for the creation of 
-   * the server instance (there is no UI in this phase).
-   *
-   * @param args [0] The port number to listen on.  Defaults to 5555
-   *          if no argument is entered.
-   */
-  public static void main(String[] args) 
-  {
-    int port = 0; //Port to listen on
+    /**
+     * This method handles any messages received from the ocsf.client.
+     *
+     * @param msg    The message received from the ocsf.client.
+     * @param client The connection from which the message originated.
+     */
+    public void handleMessageFromClient
+    (Object msg, ConnectionToClient client) {
+        serverUI.display("Message received: " + msg + " from " + client);
+        this.sendToAllClients(msg);
+    }
 
-    try
-    {
-      port = Integer.parseInt(args[0]); //Get port from command line
+    /**
+     * This method overrides the one in the superclass.  Called
+     * when the server starts listening for connections.
+     */
+    protected void serverStarted() {
+        System.out.println
+                ("Server listening for connections on port " + getPort());
     }
-    catch(Throwable t)
-    {
-      port = DEFAULT_PORT; //Set port to 5555
+
+    //Class methods ***************************************************
+
+    /**
+     * This method overrides the one in the superclass.  Called
+     * when the server stops listening for connections.
+     */
+    protected void serverStopped() {
+        System.out.println
+                ("Server has stopped listening for connections.");
     }
-	
-    EchoServer sv = new EchoServer(port);
-    
-    try 
-    {
-      sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
-    {
-      System.out.println("ERROR - Could not listen for clients!");
-    }
-  }
 }
 //End of EchoServer class
---
+++ src/simplechat1/ServerConsole.java
@@ -0,0 +1,65 @@
+package simplechat1;
+
+import simplechat1.common.ChatIF;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ServerConsole implements ChatIF {
+    EchoServer server;
+
+    //constructor
+    public ServerConsole(int port) {
+        try {
+            server = new EchoServer(port, this);
+        } catch (Exception e) {
+            display("An error has occurred and EchoServer could not be started. Terminating");
+            System.exit(1);
+        }
+    }
+
+    //class methods
+    @Override
+    public void display(String message) {
+        System.out.println("> " + message);
+    }
+
+    public void accept() {
+        try {
+            BufferedReader fromConsole =
+                    new BufferedReader(new InputStreamReader(System.in));
+            String message;
+
+            while (true) {
+                message = fromConsole.readLine();
+                server.handleMessageFromServer(message);
+            }
+        } catch (Exception ex) {
+            System.out.println
+                    ("Unexpected error while reading from console!");
+        }
+    }
+
+    //instance methods
+    public static void main(String[] args) {
+        int port = 0;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            port = EchoServer.DEFAULT_PORT;
+        }
+
+        ServerConsole svrCon = new ServerConsole(port);
+
+        try {
+            svrCon.server.listen();
+        } catch (IOException e) {
+            svrCon.display("Couldn't listen for clients");
+        }
+
+        svrCon.accept();
+
+
+    }
+}
